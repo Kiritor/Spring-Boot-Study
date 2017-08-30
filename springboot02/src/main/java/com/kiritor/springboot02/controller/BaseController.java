cc443cf707c2b45d53d476a24133b6c6661f788e
Kiritor <1406754158@qq.com>
2017-08-30 14:49:06 +0800

    快速入门(基本项目构建,加入web模块,简单配置,可作为脚手架工程)

--- springboot02/src/main/java/com/kiritor/springboot02/controller/BaseController.java
+++ springboot02/src/main/java/com/kiritor/springboot02/controller/BaseController.java
@@ -1,5 +1,4 @@
-package com.kiritor.springboot01.controller;
-
+package com.kiritor.springboot02.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BaseController {
 
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     public String hello(){
-
         return "hello Springboot";
-        
     }
-
 }
