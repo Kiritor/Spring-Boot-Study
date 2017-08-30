f3e29d898d1c6260a4fe0a94a7426f72017aa7f9
Kiritor <1406754158@qq.com>
2017-08-30 14:51:51 +0800

    war包发布(基本项目构建,加入web模块,简单配置,可作为war包形式的脚手架工程)

---
+++ springboot02/src/main/java/com/kiritor/springboot02/Application.java
@@ -0,0 +1,19 @@
+package com.kiritor.springboot02;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    public static void main(String[] args) {
+        //TEST
+        //SpringApplication.run(Application.class, args);
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
+        //修改Banner的模式为OFF
+        builder.bannerMode(Banner.Mode.CONSOLE).run(args);
+    }
+}
